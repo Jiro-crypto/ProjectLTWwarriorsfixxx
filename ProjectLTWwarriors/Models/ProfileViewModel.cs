49a5e769ec1604e89cc6b5b0f6e9207c2a8dca91
Legion <Legion@DESKTOP-VPHEA6L>
2025-10-31 21:34:33 +0700

    Add project files.

---
+++ ProjectLTWwarriors/Models/ProfileViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectLTWwarriors.Models
+{
+    public class ProfileViewModel
+    {
+        public User User { get; set; }
+        public List<DonHang> DonHangs { get; set; }
+    }
+
+}
